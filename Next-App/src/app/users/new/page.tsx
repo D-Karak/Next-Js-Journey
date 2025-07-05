49cf69c121d6d869e28acbc3dc9f0ec9eda930f2
D-Karak <dibyendu.karak05@gmail.com>
2025-07-05 21:18:47 +0530

    initial commit-setup next app

---
+++ Next-App/src/app/users/new/page.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const NewUserPage = () => {
+  return (
+    <>
+    <div>NewUserPage</div>
+    <a href="/Users">Users Page</a>
+    </>
+  )
+}
+
+export default NewUserPage
---
+++ Next-App/src/app/users/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <>
+    <div>UserPage</div>
+    <Link href="/">Homepage</Link>
+    </>
+  )
+}
+
+export default UserPage
