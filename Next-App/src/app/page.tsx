8808b3374a7704257d211145b955c5c842d255d1
D-Karak <dibyendu.karak05@gmail.com>
2025-07-17 19:27:05 +0530

    initial commit for a G-auth app with role based routing

--- Next-App/src/app/page.tsx
+++ Next-App/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import { SignInButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="p-10">
         <h1 >Hello World</h1>
         <Link href="/users">Users page</Link><br />
         <Link href="/users/new">New User page</Link>
+        <SignInButton mode="modal"/>
     </div>
   );
 }
